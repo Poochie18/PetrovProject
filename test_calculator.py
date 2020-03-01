e5f8f89c1afbec1485bea99a66ab9963db1ce0c5
Poochie18 <39067344+Poochie18@users.noreply.github.com>
2020-03-01 11:48:01 +0200

    version 1.02

--- test_calculator.py
+++ test_calculator.py
@@ -5,12 +5,15 @@ from calc import Calculator
 class TestCalculator(TestCase):
 
     def test_div(self):
+        a = Calculator(2,-4)
         b = Calculator(4,-4)
         c = Calculator(1, 1000000000)
         self.assertEqual(Calculator.div(b.first,b.second), -1)
         self.assertEqual(Calculator.div(b.first, b.second), -1.0)
         self.assertEqual(Calculator.div(b.first, b.second), -1.0000000000000001)
         self.assertEqual(Calculator.div(c.first, c.second), 0.000000001)
+        self.assertEqual(Calculator.add(a,b),-1.5)
+        self.assertEqual(Calculator.simply(a.second, b.second), 4)
 
     def test_simply(self):
         print('Hello')
